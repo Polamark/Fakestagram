608129755f8e88df5f713a05502586c24ed68db4
OtherLinux <mark@cuprum.uk>
2025-10-10 13:22:39 +0200

    Init

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+$post = [
+    'title' => 'My First Post',
+    'content' => 'This is my first post.',
+    'tags' => ['php', 'javascript'],
+    'author' => 'John Doe',
+    'authorPFP' => "https://placehold.co/100x100",
+    'authorVerified' => true,
+    'postImg' => "https://placehold.co/400x600",
+    'likes' => 100,
+    'comments' => [
+        ['author' => 'John', 'content' => 'Great post!'],
+        ['author' => 'Jane', 'content' => 'I love it!']
+    ]
+];
+$posts = [$post]
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fakestagram</title>
+    <link rel="stylesheet" href="output.css">
+</head>
+<body>
+    <section class="w-screen h-fit">
+        <?php foreach ($posts as $post): ?>
+        <?php endforeach ?>
+    </section>
+</body>
+</html>
